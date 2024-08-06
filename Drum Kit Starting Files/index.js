6c79a6d7b3d7f11257a27dddebb15243fd85798c
Aditya Mahapatra <adinmahapatra03@gmail.com>
2024-08-07 00:43:14 +0530

    using the command this

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,7 +4,13 @@ for (var i =0; i<numberOfdrumButtons; i++){
 document.querySelectorAll("button")[i].addEventListener("click",function () {
    
     //what to do when click detected.
-    var audio = new Audio('sounds/tom-1.mp3')
-    audio.play();
+
+    this.style.color = "white";
+
+
+   
 } );
 }
+
+// var audio = new Audio('sounds/tom-1.mp3')
+// audio.play();
