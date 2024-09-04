efd23c979fba1d57f2e5956d9f29b5dc5350ec4e
Aditya Mahapatra <adinmahapatra03@gmail.com>
2024-09-04 21:59:03 +0530

    drum kit set

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,13 +4,53 @@ for (var i =0; i<numberOfdrumButtons; i++){
 document.querySelectorAll("button")[i].addEventListener("click",function () {
    
     //what to do when click detected.
+    var buttonInnerHTML = this.innerHTML;
 
-    this.style.color = "white";
+    switch(buttonInnerHTML){
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
 
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+
+            break;
+
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+
+            break;
+        case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+
+            break;
+        
+
+        default:
+            console.log();
+    }
 
    
 } );
 }
 
-// var audio = new Audio('sounds/tom-1.mp3')
-// audio.play();
+
