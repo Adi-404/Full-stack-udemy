6b5be68a0cdbd03c4fa06cc817064a63f54cb265
Aditya Mahapatra <adinmahapatra03@gmail.com>
2024-09-09 23:55:11 +0530

    adding animations

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -70,4 +70,12 @@ function makeSound (key){
 
 function buttonAnimation(currentKey){
 
+    var activeButton = document.querySelector("."+currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+
+    }, 100);
 }
